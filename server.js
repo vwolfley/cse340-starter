7532ad7ae4741fba569304a8c6cbcf658ce72836
vern wolfley <vern.wolfley@gmail.com>
2024-11-02 16:29:44 -0700

    updates

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ app.set("layout", "./layouts/layout"); // not at views root
  * Routes
  *************************/
 app.use(static);
-
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
