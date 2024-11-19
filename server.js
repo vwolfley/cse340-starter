b0ff6de04161611978d4175adbed88a79a2405c9
vern wolfley <vern.wolfley@gmail.com>
2024-11-19 12:01:13 -0700

    week 2 learning activities done

--- server.js
+++ server.js
@@ -5,37 +5,60 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
-const app = express();
-const static = require("./routes/static");
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const env = require('dotenv').config()
+const app = express()
+const static = require('./routes/static')
+const baseController = require('./controllers/baseController')
+const inventoryRoute = require('./routes/inventoryRoute')
+const utilities = require('./utilities/')
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set('layout', './layouts/layout') // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static);
+app.use(static)
 // Index route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
+app.get('/', utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use('/inv', inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+    next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+    let nav = await utilities.getNav()
+    console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+    if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+    res.render("errors/error", {
+      title: err.status || 'Server Error',
+      message,
+      nav
+    })
+  })
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
+    console.log(`app listening on ${host}:${port}`)
+})
