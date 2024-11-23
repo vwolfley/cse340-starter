a84f492c0c2e3b237f643c7a5fcdb38952b67706
vern wolfley <vern.wolfley@gmail.com>
2024-11-22 23:18:31 -0700

    fix require

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require('./utilities/')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
