c61231a711a821394660ff6f22347f8f397bf63f
vern wolfley <vern.wolfley@gmail.com>
2024-11-26 21:06:49 -0700

    update form validation

--- models/account-model.js
+++ models/account-model.js
@@ -6,7 +6,7 @@ const pool = require('../database/')
 async function registerAccount(account_firstname, account_lastname, account_email, account_password) {
     try {
         const sql =
-            "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
+            'INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, "Client") RETURNING *'
         return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
     } catch (error) {
         return error.message
